744780237ec809b1ee5b9f15e6b49597b9a42d4a
xxRohitxx <rohitrs319791@gmail.com>
2023-03-15 12:49:24 +0530

    added backend for the be social

---
+++ models/post.model.js
@@ -0,0 +1,34 @@
+const { default: mongoose } = require("mongoose");
+const postScema=mongoose.Schema({
+    title:{
+       require:true,
+       type: String
+    },
+    description:{
+        require:true,
+        type:String
+        
+    },
+    photo:{
+        require:false,
+        type:String
+
+    },
+    category:{
+        require:true,
+       
+        type:String
+
+    }
+    ,username:{
+        type:String,
+        require:true
+    },
+    postTime:{
+        type:String,
+        default:Date.now()
+    }
+})
+
+const PostModel=mongoose.model("post",postScema)
+module.exports={PostModel}
---
+++ routes/posts.route.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const { UserModel } = require("../models/user.model");
+const { PostModel } = require("../models/post.model");
+const PostRoute = express.Router();
+
+//new post by user
+PostRoute.post("/", async (req, resp) => {
+  const newPost = new PostModel(req.body);
+
+  try {
+    const result = await newPost.save();
+
+    resp.status(200).json(result);
+  } catch (error) {
+    resp.status(500).send(error);
+  }
+});
+
+//update post by the user
+
+PostRoute.patch("/:id", async (req, resp) => {
+  try {
+    const post = await PostModel.findById(req.params.id);
+    if (post.username == req.body.username) {
+      try {
+        const updatePost = await PostModel.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          {
+            new: true,
+          }
+        );
+
+        resp.status(200).json(updatePost);
+      } catch (err) {
+        resp.status(401).json(err);
+      }
+    } else {
+      resp.status(401).json("You Cannot Update this post");
+    }
+  } catch (error) {
+    resp.status(401).json(error);
+  }
+});
+
+//delete post by the user
+
+PostRoute.put("/:id", async (req, resp) => {
+  console.log(req.body)
+  try {
+    const post = await PostModel.findById(req.params.id);
+    if (post.username == req.body.username) {
+      try {
+        await PostModel.findByIdAndDelete(req.params.id);
+
+        resp.status(200).json("post Delted successfull");
+      } catch (err) {
+        resp.status(401).json(err);
+      }
+    } else {
+      resp.status(401).json("You Cannot Delete this post");
+    }
+  } catch (error) {
+    resp.status(401).json(error);
+  }
+});
+
+PostRoute.get("/:id", async (req, resp) => {
+  try {
+    const post = await PostModel.findById(req.params.id);
+
+    resp.status(200).json(post);
+  } catch (error) {
+    resp.status(401).json(error);
+  }
+});
+
+PostRoute.get("/", async (req, resp) => {
+  const username = req.query.user;
+  const categoryName = req.query.cat;
+  var posts;
+  try {
+    if (username) {
+      posts = await PostModel.find({ username }).sort({'postTime': 'desc'})
+    } else if (categoryName) {
+      posts = await PostModel.find({category: { $in: [categoryName]}}).sort({'postTime': 'desc'})
+    }else{
+        posts =await PostModel.find().sort({'postTime': 'desc'})
+    }
+
+    resp.status(200).json(posts);
+  } catch (error) {
+    resp.status(401).json(error);
+  }
+});
+
+module.exports = { PostRoute };
